6866b2da4dbab01c0e351e00059ebed5581ed344
Dylan Barratt <dylanbarratt03@gmail.com>
2025-01-08 20:33:54 +0000

    feat: generate a conflicting file

---
+++ create-conf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir conf
+cd conf || exit 0
+git init
+echo -e "hello world\n\nhello unchanged\n\nthis is changes on\nmulti\nlines" >file1.txt
+git add file1.txt
+git commit -m "batman"
+git branch changes
+echo -e "hello change\n\nhello unchanged\n\nthis is nothing on\nmultdsajsjahi\nldsjadjshines" >file1.txt
+git add file1.txt
+git commit -m "change1"
+git switch changes
+echo -e "hello diff\n\nhello unchanged\n\nthis is something on\nkdjsshajhdsaj\nhiendline" >file1.txt
+git add file1.txt
+git commit -m "change2"
+git merge master
