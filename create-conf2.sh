59ebd1b67025bea828fb9f49aff0fc4625101263
Dylan Barratt <dylanbarratt03@gmail.com>
2025-01-26 14:19:27 +0000

    look nicer

--- create-conf2.sh
+++ create-conf2.sh
@@ -5,15 +5,15 @@ rm -rf conf
 mkdir conf
 cd conf || exit 0
 git init
-echo -e "local function say_hello()\nprint('Hello, World!')\nend\n-- Call the function\nsay_hello()" >hello.lua
+echo -e "local function say_hello()\n   print('Hello, World!')\nend\n-- Call the function\nsay_hello()" >hello.lua
 git add hello.lua
 git commit -m "batman"
 git branch changes
-echo -e "local function say_change()\nprint('Hello, change!')\nend\n-- Call the function\nsay_change()" >hello.lua
+echo -e "local function say_change()\n   print('Hello, change!')\nend\n-- Call the function\nsay_change()" >hello.lua
 git add hello.lua
 git commit -m "change1"
 git switch changes
-echo -e "local function say_name(name)\nprint('Hello,' .. name .. '!')\nend\n-- Call the function\nsay_name('Dylan')" >hello.lua
+echo -e "local function say_name(name)\n   print('Hello,' .. name .. '!')\nend\n-- Call the function\nsay_name('Dylan')" >hello.lua
 git add hello.lua
 git commit -m "change2"
 git merge master
